f837615ddfe310583b81c471a4b87d4ceaed1d69
Ivan Constantin Raul <constantinraulivan@gmail.com>
2022-08-06 15:49:32 +0300

    first teams list version

---
+++ index.js
@@ -0,0 +1,30 @@
+function displayTeams(teams) {
+  // transforma din json in html
+  var teamsHTML = "";
+  teams.forEach(function (team) {
+    teamsHTML += `  <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td></td>
+  </tr>`;
+  });
+
+  //afisare
+  document.querySelector("table tbody").innerHTML = teamsHTML;
+}
+
+function loadTeams() {
+  fetch("data/teams.json")
+    .then(function (r) {
+      return r.json();
+    })
+    .then(function (teams) {
+      displayTeams(teams);
+    });
+}
+
+loadTeams();
