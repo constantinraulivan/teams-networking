31cae48bd9a48b444d916691a377dd555c67c869
Ivan Constantin Raul <constantinraulivan@gmail.com>
2022-08-08 20:51:55 +0300

    add $ shortcut to querySelector

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function $(selector) {
+  return document.querySelector(selector);
+}
+
 function getTeamHTML(team) {
   return `  <tr>
   <td>${team.promotion}</td>
@@ -13,7 +17,7 @@ function getTeamHTML(team) {
 function displayTeams(teams) {
   var teamsHTML = teams.map(getTeamHTML);
   //afisare
-  document.querySelector("table tbody").innerHTML = teamsHTML.join("");
+  $("table tbody").innerHTML = teamsHTML.join("");
 }
 
 function loadTeams() {
@@ -27,10 +31,10 @@ function loadTeams() {
 }
 
 function submitForm(e) {
-  var promotion = document.querySelector("input[name=promotion]").value;
-  var members = document.querySelector("input[name=members]").value;
-  var name = document.querySelector("input[name=name]").value;
-  var url = document.querySelector("input[name=url]").value;
+  var promotion = $("input[name=promotion]").value;
+  var members = $("input[name=members]").value;
+  var name = $("input[name=name]").value;
+  var url = $("input[name=url]").value;
 
   var team = {
     promotion: promotion,
